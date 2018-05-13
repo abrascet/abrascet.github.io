eb5d559737dd890a37dfdc00598b0a890a89a2df
abrascet <39135997+abrascet@users.noreply.github.com>
2018-05-13 19:13:13 +0200

    added resize handler

--- asset/js/abrascet.js
+++ asset/js/abrascet.js
@@ -2,14 +2,21 @@
 (function() {
   var navToggle = document.getElementById("navigation-toggle");
   var navList = document.getElementById("navigation-list");
-  navToggle.addEventListener('click', toggle);
+  navToggle.addEventListener('click', toggleHandler);
+  window.addEventListener('resize', resizeHandler);
 
-	function toggle(event) {
+	function toggleHandler(event) {
 		event.preventDefault();
     if (navList.style.display === "none") {
       navList.style.display = "block";
     } else {
       navList.style.display = "none";
+    }
+  }
+
+  function resizeHandler(event) {
+    if (navToggle.style.display === "none") {
+      navList.style.display = "block";
     }
   }
 
