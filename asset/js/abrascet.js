106e2a6d33a80d9fdf81ebd129e6410a5183e6b9
abrascet <39135997+abrascet@users.noreply.github.com>
2021-04-23 14:13:24 +0200

    Fix googleoff/googleon tags

--- asset/js/abrascet.js
+++ asset/js/abrascet.js
@@ -74,7 +74,7 @@
     var message = "Az oldalon történő látogatása során cookie-kat használunk, melyek információkat szolgáltatnak számunkra a felhasználó oldallátogatási szokásairól, de nem tárolnak személyes információkat. Az oldalon történő továbblépéssel elfogadja a cookie-k használatát.";
     var consentContainer = document.createElement("div");
     consentContainer.style.cssText = "margin:.4em;padding:.9em;max-width:24em;position:fixed;bottom:0;right:0;overflow:hidden;color:#ffffff;background:#252e39;z-index:1999;";
-    consentContainer.innerHTML = '<div>' + message + '</div><a id="consent-close" class="button" style="margin:1em 0 0 0;width:100%;" href="#">Bezár</a>';
+    consentContainer.innerHTML = '<!--googleoff: all--><div>' + message + '</div><a id="consent-close" class="button" style="margin:1em 0 0 0;width:100%;" href="#">Bezár</a> <!--googleon: all-->';
     footer.appendChild(consentContainer);
     document.getElementById("consent-close").addEventListener("click", function(event) {
       event.preventDefault();
