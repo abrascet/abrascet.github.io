e3b4c2b95bd7c5b9f31ddf37504c2a2e2b3afb7f
Gabor Bata <bata.gabor@gmail.com>
2022-11-22 15:12:28 +0100

    Added fallback support for image magick webp conversion

--- generator/convertwebp.rb
+++ generator/convertwebp.rb
@@ -1,8 +1,12 @@
 # convert images to webp
-webp_support = system('cwebp')
+CONVERTERS = {
+  'cwebp'          => '-q 80 %s -o %s',
+  'magick convert' => '-quality 80 %s %s'
+}
+converter = CONVERTERS.find { |cmd, params| system("#{cmd} -version") }
 Dir.glob('../asset/orig/abra/*.png').each do |image|
   webp_img = '../asset/img/abra/' + File.basename(image).sub('.png', '.webp')
-  if webp_support && !File.exist?(webp_img)
-    system("cwebp -q 80 #{image} -o #{webp_img}")
+  if converter && !File.exist?(webp_img)
+    system("#{converter[0]} #{sprintf(converter[1], image, webp_img)}")
   end
 end
